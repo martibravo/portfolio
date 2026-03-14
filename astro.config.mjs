54bb2cc206fb958791fff7426cda7556c10473e4
Martí Bravo <bravonavarro.marti@gmail.com>
2026-03-14 13:36:41 +0100

    Add @astrojs/react for Keystatic admin UI

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import mdx from '@astrojs/mdx';
 import cloudflare from '@astrojs/cloudflare';
 import keystatic from '@keystatic/astro';
 
+import react from '@astrojs/react';
+
 export default defineConfig({
   site: 'https://universitat.studio',
   adapter: cloudflare(),
-  integrations: [mdx(), keystatic()],
+  integrations: [mdx(), keystatic(), react()],
 });
